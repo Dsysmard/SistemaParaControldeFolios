7ced2dcc53bf2d4a8f15a42809ec701d0e965546
Miguel Angel Reyna <miguelangelreyna@MacBook-Pro-de-Implan.local>
2018-08-30 16:50:11 -0500

    Creacion de la tabla para el registro y control de Folios

---
+++ database/migrations/2018_08_30_212825_create_folios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('folios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('num_folio');
+            $table->string('nombre_creador');
+            $table->string('nom_departamento_emitido');
+            $table->string('nom_dependecia_dirigido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('folios');
+    }
+}
